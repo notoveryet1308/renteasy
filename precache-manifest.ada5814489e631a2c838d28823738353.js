e6019fd10e25e4a12796dc50837ff4ecebeda73e
rahul <rahulraz1308@gmail.com>
2020-04-07 20:26:55 +0530

    Updates

---
+++ precache-manifest.ada5814489e631a2c838d28823738353.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85c2e04f74a24a3720d5bb714eecd5b5",
+    "url": "/renteasy/index.html"
+  },
+  {
+    "revision": "19743f45189658d5b584",
+    "url": "/renteasy/static/css/main.6b91ad73.chunk.css"
+  },
+  {
+    "revision": "61390dde1d90700e62ce",
+    "url": "/renteasy/static/js/2.d39ccaf6.chunk.js"
+  },
+  {
+    "revision": "85701e1121fb954a406d391321a3de9e",
+    "url": "/renteasy/static/js/2.d39ccaf6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19743f45189658d5b584",
+    "url": "/renteasy/static/js/main.55bd9eb7.chunk.js"
+  },
+  {
+    "revision": "9894d163010997bd1908",
+    "url": "/renteasy/static/js/runtime-main.e3d31d56.js"
+  },
+  {
+    "revision": "109d5f13a1144ca6c9cc6218f9f4e0db",
+    "url": "/renteasy/static/media/Business.com_Logo.109d5f13.svg"
+  },
+  {
+    "revision": "10a1d9caea507d6e9c621e5d40db87a7",
+    "url": "/renteasy/static/media/ac.10a1d9ca.svg"
+  },
+  {
+    "revision": "0948b674f184f91c67262a8a99315394",
+    "url": "/renteasy/static/media/agrawebp.0948b674.webp"
+  },
+  {
+    "revision": "20704e352a6dcf03c792797a5147755c",
+    "url": "/renteasy/static/media/amritsarwebp.20704e35.webp"
+  },
+  {
+    "revision": "0a3ba7ffb98b5528603fb006493bf0b5",
+    "url": "/renteasy/static/media/assamwebp.0a3ba7ff.webp"
+  },
+  {
+    "revision": "b1bda64326279416ba3e1d5de76c4aee",
+    "url": "/renteasy/static/media/building-3.b1bda643.jpg"
+  },
+  {
+    "revision": "e695bd70e876fe1ec3a799ed06125941",
+    "url": "/renteasy/static/media/chandigarhwebp.e695bd70.webp"
+  },
+  {
+    "revision": "777b93c6d5a7b86081144f8d0d3ca1d8",
+    "url": "/renteasy/static/media/clear-white.777b93c6.svg"
+  },
+  {
+    "revision": "c49bc50d60a79666aa00664fff692a50",
+    "url": "/renteasy/static/media/custom-offer.c49bc50d.png"
+  },
+  {
+    "revision": "74030b39c0c1057b256e3330210c7565",
+    "url": "/renteasy/static/media/custom-offer3@2x.74030b39.png"
+  },
+  {
+    "revision": "d77d9c5eed977671bec2a445b9dfaa1b",
+    "url": "/renteasy/static/media/custom-offer4@2x.d77d9c5e.png"
+  },
+  {
+    "revision": "f129fb1e1582b22bf0dc1cbff878011f",
+    "url": "/renteasy/static/media/delhiwebp.f129fb1e.webp"
+  },
+  {
+    "revision": "0d3c0a536c6741512a04f9680d42ecfd",
+    "url": "/renteasy/static/media/dishwasher.0d3c0a53.svg"
+  },
+  {
+    "revision": "f881e517a427fc4beb2b59f599d9ad25",
+    "url": "/renteasy/static/media/doorman.f881e517.svg"
+  },
+  {
+    "revision": "8acf63e47585b839e90a8288895392c7",
+    "url": "/renteasy/static/media/elevator.8acf63e4.svg"
+  },
+  {
+    "revision": "a5d73fe85434e1865f5b14d2622c400c",
+    "url": "/renteasy/static/media/equities.a5d73fe8.png"
+  },
+  {
+    "revision": "d01110bcbd72961ad1416fb3b60579fb",
+    "url": "/renteasy/static/media/floor.d01110bc.svg"
+  },
+  {
+    "revision": "c5062b1ec9d9fc4895fd143b5a2c01f3",
+    "url": "/renteasy/static/media/forbes.c5062b1e.svg"
+  },
+  {
+    "revision": "22a55d04d1511ab90a2c737301ef803c",
+    "url": "/renteasy/static/media/gurugramwebp.22a55d04.webp"
+  },
+  {
+    "revision": "21d3a51ebbd771e7314001be1b69c4ea",
+    "url": "/renteasy/static/media/homeowner-screening-1.21d3a51e.png"
+  },
+  {
+    "revision": "397636c8ee557203df388c15f59065ae",
+    "url": "/renteasy/static/media/homeowner1-1.397636c8.png"
+  },
+  {
+    "revision": "6fb2c0797d24448705858e331763f1f3",
+    "url": "/renteasy/static/media/homeowner1-2.6fb2c079.png"
+  },
+  {
+    "revision": "270394157b84f5236fa85d101b99676e",
+    "url": "/renteasy/static/media/homeowner2-1.27039415.png"
+  },
+  {
+    "revision": "c046fcde740815d9f3233e0fe662b298",
+    "url": "/renteasy/static/media/homeowner5-.c046fcde.png"
+  },
+  {
+    "revision": "cebb875a9242e2e9467a279adff7db80",
+    "url": "/renteasy/static/media/homeowner6-1.cebb875a.png"
+  },
+  {
+    "revision": "c908c42e4d73913844d71ce013f7b474",
+    "url": "/renteasy/static/media/homeowner6-2.c908c42e.png"
+  },
+  {
+    "revision": "73d6f23326237d9d2fdac913f241fcba",
+    "url": "/renteasy/static/media/house-0.73d6f233.jpg"
+  },
+  {
+    "revision": "b86d8aacef45c24b28db1bc1c422bcd4",
+    "url": "/renteasy/static/media/house-1.b86d8aac.jpg"
+  },
+  {
+    "revision": "5d40ac55d3e14a7a9dd34d0a576b1a06",
+    "url": "/renteasy/static/media/house-2.5d40ac55.jpg"
+  },
+  {
+    "revision": "64d4f65015fc3d4db16a0182ba6718b8",
+    "url": "/renteasy/static/media/house-3.64d4f650.jpg"
+  },
+  {
+    "revision": "f6b51f197f5de0bc6716e15969d9b66c",
+    "url": "/renteasy/static/media/house-4.f6b51f19.jpg"
+  },
+  {
+    "revision": "78f5ace1adaeac898b57c2d70c59efe3",
+    "url": "/renteasy/static/media/house-5.78f5ace1.jpg"
+  },
+  {
+    "revision": "335785150902b40993ff959efea05a5e",
+    "url": "/renteasy/static/media/house-6.33578515.jpg"
+  },
+  {
+    "revision": "80c016ba0c194d3d015e10d5ce5cf280",
+    "url": "/renteasy/static/media/house-7.80c016ba.jpg"
+  },
+  {
+    "revision": "b60bfa97eb3d0051ae437959ba2c767f",
+    "url": "/renteasy/static/media/house-8.b60bfa97.jpg"
+  },
+  {
+    "revision": "b33d99d155c6caf8358c69c11024f8ad",
+    "url": "/renteasy/static/media/house-9.b33d99d1.jpg"
+  },
+  {
+    "revision": "620b408873d7bbc849583e76bb2cc251",
+    "url": "/renteasy/static/media/inman-logo.620b4088.svg"
+  },
+  {
+    "revision": "5cfd80fd560e31c6719e38f7def97aa9",
+    "url": "/renteasy/static/media/int_1.5cfd80fd.jpg"
+  },
+  {
+    "revision": "d43fb53cef403e6a446f392ae1d1d86c",
+    "url": "/renteasy/static/media/int_2.d43fb53c.jpg"
+  },
+  {
+    "revision": "4b61150074e05b929edab7ec44ccc4ae",
+    "url": "/renteasy/static/media/int_3.4b611500.jpg"
+  },
+  {
+    "revision": "3ba6ea05580972fc522ff0b8258b310c",
+    "url": "/renteasy/static/media/ladakhwebp.3ba6ea05.webp"
+  },
+  {
+    "revision": "0b42a7d102dbafbbc9c6b635149f23ba",
+    "url": "/renteasy/static/media/money.0b42a7d1.png"
+  },
+  {
+    "revision": "12902debc33db678db1406a6ec8544fe",
+    "url": "/renteasy/static/media/parking.12902deb.svg"
+  },
+  {
+    "revision": "a1e0b849c3acc8040aacd6f96c82e013",
+    "url": "/renteasy/static/media/patch.a1e0b849.svg"
+  },
+  {
+    "revision": "4bf1193b4bb8bcf417ff1a478e5bfa43",
+    "url": "/renteasy/static/media/patnawebp.4bf1193b.webp"
+  },
+  {
+    "revision": "0e77920fea9ffeb1b96d5bdedec1706b",
+    "url": "/renteasy/static/media/pix.0e77920f.png"
+  },
+  {
+    "revision": "76a369ff74e190c8374dd99fc926f19f",
+    "url": "/renteasy/static/media/quality.76a369ff.png"
+  },
+  {
+    "revision": "e953f7639452ca8889d00de7f1fe5eb3",
+    "url": "/renteasy/static/media/shimla.e953f763.jpg"
+  },
+  {
+    "revision": "de8f009ebf4dd0949a7e5224dddebc29",
+    "url": "/renteasy/static/media/speaker.de8f009e.png"
+  },
+  {
+    "revision": "fd53a4f0f4486765bc4a50c30f66386e",
+    "url": "/renteasy/static/media/storage.fd53a4f0.svg"
+  },
+  {
+    "revision": "b5491d80c515d39948ea2b157d0aa605",
+    "url": "/renteasy/static/media/tenant1-1.b5491d80.png"
+  },
+  {
+    "revision": "6dcb942cf852bdffa290df279678228f",
+    "url": "/renteasy/static/media/tenant1-2.6dcb942c.png"
+  },
+  {
+    "revision": "a54ede2172fdb6d15f8c3e9c1042e074",
+    "url": "/renteasy/static/media/tenant3-1.a54ede21.png"
+  },
+  {
+    "revision": "54cbbb4de97946a9a00935355f0e9714",
+    "url": "/renteasy/static/media/tenant3-2.54cbbb4d.png"
+  },
+  {
+    "revision": "96a6377b301c634a65a16dc0db0d3d37",
+    "url": "/renteasy/static/media/tenant5-1.96a6377b.png"
+  },
+  {
+    "revision": "dd36b327143be02d37bd5ed87c93e22e",
+    "url": "/renteasy/static/media/tenant5-2.dd36b327.png"
+  },
+  {
+    "revision": "c0061dc80ad52fe1e2d162a3613e0a9c",
+    "url": "/renteasy/static/media/texture_3.c0061dc8.jpg"
+  },
+  {
+    "revision": "3b0bd028b885a1a86c27b1293ee8013f",
+    "url": "/renteasy/static/media/view.3b0bd028.svg"
+  }
+]);
